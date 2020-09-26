efe506e62d1632e26222ffd247c688b2f3db081b
unknown <shahzebmomin@gmail.com>
2020-09-25 20:44:57 -0500

    assignment 4

--- site/Assignment3/SpeakGoodBye.js
+++ site/Assignment3/SpeakGoodBye.js
@@ -10,7 +10,7 @@
 // See Lecture 52, part 1
 // var byeSpeaker =
 (function(window){
-    var byeSpeaker = new Object();
+    var byeSpeaker = {};
     var speakWord = "Good Bye";
     byeSpeaker.speak = function(name){
         console.log(speakWord+" "+name);
